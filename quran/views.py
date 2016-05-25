5e08d91e204ba5aca4f2ce364967475bea79fe17
Mehmet <mehmet@edgle.com>
2016-05-25 22:54:12 +0000

    more detailed root view

--- quran/views.py
+++ quran/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Prefetch
-from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response
+from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response, redirect
 
 from quran.models import *
 
@@ -20,23 +20,29 @@ def get_aya(request, sura_number, aya_number, template_name='quran/aya.html'):
 
 
 def get_word(request, sura_number, aya_number, word_number, template_name='quran/word.html'):
-    aya = Aya.objects.filter(sura__number=sura_number, number=aya_number)\
+    aya = Aya.objects.filter(sura__number=sura_number, number=aya_number) \
         .prefetch_related(prefetch_aya_translations(request))
     word = Word.objects.filter(aya=aya, number=word_number).prefetch_related()  # todo cant see extent of data brought
-    ayas = Aya.objects.filter(words__utext=word[0].utext)\
-        .order_by('sura_id', 'number')\
-        .prefetch_related(prefetch_aya_translations(request)) # other ayas with same word
-    return render_to_response(template_name, {'word': word[0], 'aya': aya[0], 'ayas': ayas })
+    ayas = Aya.objects.filter(words__utext=word[0].utext) \
+        .order_by('sura_id', 'number') \
+        .prefetch_related(prefetch_aya_translations(request))  # other ayas with same word
+    return render_to_response(template_name, {'word': word[0], 'aya': aya[0], 'ayas': ayas})
 
 
 def get_lemma(request, lemma_id, template_name='quran/lemma.html'):
     lemma = get_object_or_404(Lemma, pk=lemma_id)
-    words = lemma.words.all().select_related('aya')
+    aya_translation_queryset = AyaTranslation.objects.filter(translation_id=get_translation_id(request))
+    words = lemma.words.all() \
+        .order_by('sura_id', 'aya_id') \
+        .prefetch_related('aya') \
+        .prefetch_related(Prefetch('aya__translations', queryset=aya_translation_queryset))
     return render_to_response(template_name, {'lemma': lemma, 'words': words})
 
 
 def get_root(request, root_id, template_name='quran/root.html'):
-    lemmas = Lemma.objects.filter(root__id=root_id).prefetch_related('words__aya')
+    lemmas = Lemma.objects.filter(root__id=root_id)\
+        .prefetch_related('words__aya')\
+        .prefetch_related(Prefetch('words__aya__translations', queryset=AyaTranslation.objects.filter(translation_id=get_translation_id(request))))
     return render_to_response(template_name, {'lemmas': lemmas})  # , 'ayas': ayas
 
 
@@ -48,14 +54,17 @@ def root_index(request, template_name='quran/root_index.html'):
 def settings(request):
     translation = request.GET.get('translation', None)
     if translation:
-        request.session['translation']=translation
+        request.session['translation'] = translation
+    return redirect('quran_index')
+
+
+def get_translation_id(request):
+    if 'translation' in request.session:
+        return request.session['translation']
+    else:
+        return 2
+
 
 def prefetch_aya_translations(request):
-    if 'translation' in request.session:
-        translation_id = request.session['translation']
-    else:
-        translation_id = 2
+    translation_id = get_translation_id(request)
     return Prefetch('translations', queryset=AyaTranslation.objects.filter(translation_id=translation_id))
-
-
-
