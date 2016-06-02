2b37babd087334ee690c41f3ebeac013f022c02f
Mehmet <mehmet@edgle.com>
2016-06-02 17:59:37 +0000

    turned function based views to CBV for easier overriding

--- quran/urls.py
+++ quran/urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import *
-from . import views
-
+from .views import *
 
 urlpatterns = [
-    url(r'^$', views.index, name='quran_index'),
-    url(r'^(?P<sura_number>\d+)/$', views.get_sura, name='quran_sura'),
-    url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/$', views.get_aya, name='quran_aya'),
-    url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/(?P<word_number>\d+)/$', views.get_word, name='quran_word'),
+    url(r'^$', IndexView.as_view(), name='quran_index'),
+    url(r'^(?P<sura_number>\d+)/$', SuraView.as_view(), name='quran_sura'),
+    url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/$', AyaView.as_view(), name='quran_aya'),
+    url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/(?P<word_number>\d+)/$', WordView.as_view(), name='quran_word'),
 
-    url(r'^page/(?P<page_number>\d+)/$', views.get_page, name='quran_page'),
+    url(r'^page/(?P<page_number>\d+)/$', PageView.as_view(), name='quran_page'),
 
-    url(r'^lemma/(?P<lemma_id>\d+)/$', views.get_lemma, name='quran_lemma'),
-    url(r'^root/(?P<root_id>\d+)/$', views.get_root, name='quran_root'),
-    url(r'^root/$', views.root_index, name='quran_root_list'),
+    url(r'^lemma/(?P<lemma_id>\d+)/$', LemmaView.as_view(), name='quran_lemma'),
+    url(r'^root/(?P<root_id>\d+)/$', RootView.as_view(), name='quran_root'),
+    url(r'^root/$', RootIndexView.as_view(), name='quran_root_index'),
 
-    url(r'^settings/$', views.settings, name='quran_settings')
+    url(r'^settings/$', settings, name='quran_settings')
 ]
--- quran/views.py
+++ quran/views.py
@@ -1,63 +1,99 @@
 from django.db.models import Prefetch
 from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response, redirect
+from django.views.generic import TemplateView
 
 from quran.models import *
 
 
-def index(request, template_name='quran/index.html'):
-    suras = get_list_or_404(Sura)
-    return render_to_response(template_name, {'suras': suras})
+class IndexView(TemplateView):
+    template_name = 'quran/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        suras = get_list_or_404(Sura)
+        context['suras'] = suras
+        return context
 
 
-def get_sura(request, sura_number, template_name='quran/sura.html'):
-    ayas = Aya.objects.filter(sura__number=sura_number)\
-        .prefetch_related(prefetch_aya_translations(request))
-    return render_to_response(template_name, {'ayas': ayas})
+class SuraView(TemplateView):
+    template_name = 'quran/sura.html'
+    def get_context_data(self, sura_number, **kwargs):
+        context = super(SuraView, self).get_context_data(**kwargs)
+        ayas = Aya.objects.filter(sura__number=sura_number)\
+            .prefetch_related(prefetch_aya_translations(self.request))
+        context['ayas'] = ayas
+        return context
 
 
-def get_page(request, page_number, template_name='quran/page.html'):
-    page = Page.objects.get(number=page_number)
-    ayas = Aya.objects.filter(id__gte=page.aya_begin_id, id__lte=page.aya_end_id)\
-        .prefetch_related(prefetch_aya_translations(request))
-    return render_to_response(template_name, {'ayas': ayas})
+class PageView(TemplateView):
+    template_name='quran/page.html'
+    def get_context_data(self, page_number, **kwargs):
+        context = super(PageView, self).get_context_data(**kwargs)
+        page = Page.objects.get(number=page_number)
+        ayas = Aya.objects.filter(id__gte=page.aya_begin_id, id__lte=page.aya_end_id)\
+            .prefetch_related(prefetch_aya_translations(self.request))
+        context['ayas'] = ayas
+        return context
 
 
-
-def get_aya(request, sura_number, aya_number, template_name='quran/aya.html'):
-    aya = Aya.objects.filter(sura__number=sura_number, number=aya_number).prefetch_related(prefetch_aya_translations(request))
-    return render_to_response(template_name, {'aya': aya[0]})
-
-
-def get_word(request, sura_number, aya_number, word_number, template_name='quran/word.html'):
-    aya = Aya.objects.filter(sura__number=sura_number, number=aya_number) \
-        .prefetch_related(prefetch_aya_translations(request))
-    word = Word.objects.filter(aya=aya, number=word_number).prefetch_related()  # todo cant see extent of data brought
-    ayas = Aya.objects.filter(words__utext=word[0].utext) \
-        .order_by('sura_id', 'number') \
-        .prefetch_related(prefetch_aya_translations(request))  # other ayas with same word
-    return render_to_response(template_name, {'word': word[0], 'aya': aya[0], 'ayas': ayas})
+class AyaView(TemplateView):
+    template_name='quran/aya.html'
+    def get_context_data(self, sura_number, aya_number, **kwargs):
+        context = super(AyaView, self).get_context_data(**kwargs)
+        aya = Aya.objects.filter(sura__number=sura_number, number=aya_number)\
+            .prefetch_related(prefetch_aya_translations(self.request))
+        context['aya'] = aya[0]
+        return context
 
 
-def get_lemma(request, lemma_id, template_name='quran/lemma.html'):
-    lemma = get_object_or_404(Lemma, pk=lemma_id)
-    aya_translation_queryset = AyaTranslation.objects.filter(translation_id=get_translation_id(request))
-    words = lemma.words.all() \
-        .order_by('sura_id', 'aya_id') \
-        .prefetch_related('aya') \
-        .prefetch_related(Prefetch('aya__translations', queryset=aya_translation_queryset))
-    return render_to_response(template_name, {'lemma': lemma, 'words': words})
+class WordView(TemplateView):
+    template_name='quran/word.html'
+    def get_context_data(self, sura_number, aya_number, word_number, **kwargs):
+        context = super(WordView, self).get_context_data(**kwargs)
+        aya = Aya.objects.filter(sura__number=sura_number, number=aya_number) \
+            .prefetch_related(prefetch_aya_translations(self.request))
+        word = Word.objects.filter(aya=aya, number=word_number).prefetch_related()  # todo cant see extent of data brought
+        ayas = Aya.objects.filter(words__utext=word[0].utext) \
+            .order_by('sura_id', 'number') \
+            .prefetch_related(prefetch_aya_translations(self.request))  # other ayas with same word
+        context['word'] = word[0]
+        context['aya'] = aya[0]
+        context['ayas'] = ayas
+        return context
 
 
-def get_root(request, root_id, template_name='quran/root.html'):
-    lemmas = Lemma.objects.filter(root__id=root_id)\
-        .prefetch_related('words__aya')\
-        .prefetch_related(Prefetch('words__aya__translations', queryset=AyaTranslation.objects.filter(translation_id=get_translation_id(request))))
-    return render_to_response(template_name, {'lemmas': lemmas})  # , 'ayas': ayas
+class LemmaView(TemplateView):
+    template_name='quran/lemma.html'
+    def get_context_data(self, lemma_id, **kwargs):
+        context = super(LemmaView, self).get_context_data(**kwargs)
+        lemma = get_object_or_404(Lemma, pk=lemma_id)
+        aya_translation_queryset = AyaTranslation.objects.filter(translation_id=get_translation_id(self.request))
+        words = lemma.words.all() \
+            .order_by('sura_id', 'aya_id') \
+            .prefetch_related('aya') \
+            .prefetch_related(Prefetch('aya__translations', queryset=aya_translation_queryset))
+        context['lemma'] = lemma
+        context['words'] = words
+        return context
 
 
-def root_index(request, template_name='quran/root_index.html'):
-    roots = Root.objects.all().order_by('utext')
-    return render_to_response(template_name, {'roots': roots})
+class RootView(TemplateView):
+    template_name='quran/root.html'
+    def get_context_data(self, root_id, **kwargs):
+        context = super(RootView, self).get_context_data(**kwargs)
+        lemmas = Lemma.objects.filter(root__id=root_id)\
+            .prefetch_related('words__aya')\
+            .prefetch_related(Prefetch('words__aya__translations', queryset=AyaTranslation.objects.filter(translation_id=get_translation_id(self.request))))
+        context['lemmas'] = lemmas  # , 'ayas': ayas
+        return context
+
+
+class RootIndexView(TemplateView):
+    template_name = 'quran/root_index.html'
+    def get_context_data(self, **kwargs):
+        context = super(RootIndexView, self).get_context_data(**kwargs)
+        roots = Root.objects.all().order_by('utext')
+        context['roots'] = roots
+        return context
 
 
 def settings(request):
